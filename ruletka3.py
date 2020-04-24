133a8db91d4386ab8e001bc3283e2407dd9c8213
BlablaOlya <63747919+BlablaOlya@users.noreply.github.com>
2020-04-24 16:17:47 +0300

    version_2

--- ruletka3.py
+++ ruletka3.py
@@ -1,6 +1,7 @@
 import random
 
-numbers = list(range(0, 1))
+win = False
+numbers = list(range(0, 37))
 print("Ваш вид ставки: внутренняя, на цвет, на дюжину")
 
 bet_type = input()
@@ -14,46 +15,48 @@ print("Ставки сделаны, ставок больше нет, спаси
 
 x = random.choice(numbers)
 
-if (x % 2) == 0:
-    color_x = "черный"
-else:
+if x in (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36):
     color_x = "красный"
+else:
+    color_x = "черный"
+if x == 0:
+    color_x = "-"
 
 if 1 < x <= 12:
-    dozen = "Первая дюжина"
+    dozen = "первая"
 if 12 < x <= 24:
-    dozen = "Вторая дюжина"
+    dozen = "вторая"
 if 24 < x <= 36:
-    dozen = "Третья дюжина"
+    dozen = "третья"
+if x == 0:
+    dozen = "-"
 
 if x == 0:
     print("Все деньги в козино")
 
-print(x)
-print(color_x)
-print(dozen)
+print("Выигрышный результат: ", x)
+print("Выигрышный цвет: ", color_x)
+print("Выигрышная дюжина: ", dozen)
 
 if bet_type == inside_bet:
     result = int(bet)
     if result == x:
-        print("Ты выиграл!")
-    else:
-        print("Ты проиграл")
+        win = True
 
 if bet_type == color_bet:
     result = str(bet)
     if result == color_x:
-        print("Ты выиграл!")
-    else:
-        print("Ты проиграл")
+        win = True
 
 if bet_type == outside_bet:
     result = str(bet)
     if result == dozen:
-        print("Ты выиграл!")
-    else:
-        print("Ты проиграл")
+        win = True
 
+if win:
+    print("Ты выиграл!")
+else:
+    print("В следующий раз повезет :-)")
 
 
 
