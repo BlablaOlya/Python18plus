c573201f7ccfed26ed2431b01ea7d58049615bce
BlablaOlya <63747919+BlablaOlya@users.noreply.github.com>
2020-04-27 14:33:08 +0300

    slovar_4les

---
+++ slovar4.py
@@ -0,0 +1,22 @@
+import random
+amount_of_words = input("Как много слов вы собираетесь занести в словарь? ")
+aow = int(amount_of_words)
+
+slovar = {}
+
+for i in range(aow):
+    key = input("Введите слово на английском\n:")
+    value = input("Введите слово на русском\n:")
+    slovar[key] = value
+print(slovar)
+
+mixed = list(slovar.keys())
+random.shuffle(mixed)
+
+for key in mixed:
+    print('Введите значение слова: ' + key)
+    answer = input("Вы считаете, что это слово переводится как\n :")
+    if slovar[key] == answer:
+        print("Все правильно")
+    else:
+        print("Бывает")
